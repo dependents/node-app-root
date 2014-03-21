7c9fdb97701ad606a7b79e7e51dc6a05aaf9cdfe
Joel Kemp <mrjoelkemp@gmail.com>
2014-03-21 13:50:30 -0400

    Fake ignored directory with a root

--- test/index.js
+++ test/index.js
@@ -1,12 +1,16 @@
 var getAppRoot = require('../');
 
-getAppRoot(__dirname + '/commonjs', function (root) {
+var options = {
+  ignore: ['bower_components']
+};
+
+getAppRoot(__dirname + '/commonjs', options, function (root) {
   console.log('CommonJS Candidate root: ', root);
   console.log(root.length === 1);
   console.log(root[0].indexOf('a2.js') !== -1);
 });
 
-getAppRoot(__dirname + '/amd', function (root) {
+getAppRoot(__dirname + '/amd', options, function (root) {
   console.log('AMD Candidate root: ', root);
   console.log(root.length === 1);
   console.log(root[0].indexOf('a2.js') !== -1);
