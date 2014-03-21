c17416daca56ea70153c45e6ad5a7e681b87d8e1
Joel Kemp <mrjoelkemp@gmail.com>
2014-03-21 12:09:53 -0400

    Restructure test/ to support amd test

--- test/commonjs/a2.js
+++ test/commonjs/a2.js
@@ -1,12 +1,9 @@
-// A file that has a bunch of built-in but no
-// custom/relative dependencies
-
-// Should not be the root node
-
 var fs = require('fs'),
     path = require('path'),
     exec = require('child_process').exec,
     http = require('http'),
+
+    // Makes this the root
     a = require('./a');
 
 console.log('Booyah');
--- test/index.js
+++ test/index.js
@@ -1,5 +1,13 @@
 var getAppRoot = require('../');
 
-getAppRoot(__dirname + '/js', function (root) {
-  console.log('Candidate root: ', root)
+getAppRoot(__dirname + '/commonjs', function (root) {
+  console.log('CommonJS Candidate root: ', root);
+  console.log(root.length === 1);
+  console.log(root[0].indexOf('a2.js') !== -1);
+});
+
+getAppRoot(__dirname + '/amd', function (root) {
+  console.log('AMD Candidate root: ', root);
+  console.log(root.length === 1);
+  console.log(root[0].indexOf('a2.js') !== -1);
 });
